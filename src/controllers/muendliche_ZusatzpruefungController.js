07ad748312d6a4c29f8357b8e41cf501c9d2ae1a
Pascal-Programming <pascal-programming@gmx.de>
2026-01-14 08:31:50 +0100

    EVAL-7: Add Muendliche_Zusatzpruefung DB connection

---
+++ src/controllers/muendliche_ZusatzpruefungController.js
@@ -0,0 +1,18 @@
+const { insertPruefungteil2 } = require('../service/muendliche_Zusatzpruefung');
+
+function addMuendliche_Zusatzpruefung(req, res) {
+  const data = req.body;
+  console.log('Controller received:', data);
+
+  insertPruefungteil2insertMuendliche_Zusatzpruefung(data, (err, result) => {
+    if (err) {
+      console.error('Insert failed:', err.message);
+      res.status(500).json({ error: 'Database insert failed', details: err.message });
+    } else {
+      console.log('Insert succeeded:', result);
+      res.status(201).json({ message: 'Muendliche Zusatzpruefung added successfully!', id: result.id });
+    }
+  });
+}
+
+module.exports = { addMuendliche_Zusatzpruefung };
